2bb85d45e1f7a1209b71d7e76a11537537ce166a
Alex Gavrilov <>
2021-02-02 17:10:09 +0300

    additionals

--- list-todo-app/src/components/taskItem/taskItem.jsx
+++ list-todo-app/src/components/taskItem/taskItem.jsx
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 import cx from 'classnames';
 
+// @TODO найти в проект use-кейсы по использованию хуков useCallback, useMemo
+// @TODO найти места где их использовать не нужно и объяснить почему
+
 const TaskItem = ({ task }) => {
   return (
-    <div className={cx({[styles.taskItem]: true, [styles.taskItem2]: false})}>
+    <div key={task.id} className={cx({[styles.taskItem]: true, [styles.taskItem2]: false})}>
       "listId": {task.listId},
       "text": {task.text},
       "date": {task.date},
